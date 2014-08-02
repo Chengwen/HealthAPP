282651706ecb6bba88db2f6dd3f21b0cf9dbb1b9
chengwen <chengwen.sui@gmail.com>
2014-08-02 15:02:21 -0400

    some

--- src/com/smartcare/healthapp/MainActivity.java
+++ src/com/smartcare/healthapp/MainActivity.java
@@ -1,13 +1,10 @@
 package com.smartcare.healthapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -15,7 +12,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main); 
-
+		
+		Button button = (Button)findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {  
+		    @Override
+		    public void onClick(View v) {
+		        Intent intent = new Intent();
+		        intent.setClass(MainActivity.this, PageBActivity.class);
+		        startActivity(intent);
+		        //设置切换动画，从右边进入，左边退出
+		        overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);                
+		    }
+		});
+        
 	}
 
 
---
+++ src/com/smartcare/healthapp/PageBActivity.java
@@ -0,0 +1,17 @@
+package com.smartcare.healthapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PageBActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main); 
+
+	}
+
+
+
+}
