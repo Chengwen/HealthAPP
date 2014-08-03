a08b6a0cd257e7d992364337efe45fcc4c72ed2a
Chengwen <chengwen.sui@gmail.com>
2014-08-03 17:59:57 -0400

    Change about page

--- src/com/smartcare/healthapp/AboutActivity.java
+++ src/com/smartcare/healthapp/AboutActivity.java
@@ -5,28 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.AnimationSet;
-import android.view.animation.AnimationUtils;
-import android.view.animation.LayoutAnimationController;
-import android.view.animation.TranslateAnimation;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ViewFlipper;
 
-public class JoinActivity extends Activity {
-
+public class AboutActivity extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		 //Remove title bar
 	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-		setContentView(R.layout.join); 
+		setContentView(R.layout.about); 
 		
-		 findViewById(R.id.continue1).setOnClickListener( new View.OnClickListener() {
+		 /*findViewById(R.id.continue1).setOnClickListener( new View.OnClickListener() {
 	            @Override
 	            public void onClick(View v) {
 	                //Inform the user the button has been clicked
@@ -35,9 +24,8 @@ public class JoinActivity extends Activity {
 			        startActivity(intent);
 			        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);   
 	            }
-	        });
+	        });*/
         
 	}
-
 
 }
