2fffa5590b18e40d852baf8dc112fdb66b8c1647
chengwen <chengwen.sui@gmail.com>
2014-08-03 10:38:17 -0400

    add first page anim

---
+++ src/com/smartcare/healthapp/FirstActivity.java
@@ -0,0 +1,147 @@
+package com.smartcare.healthapp;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
+
+public class FirstActivity extends Activity {
+
+    Timer timer = new Timer();  
+    ViewFlipper vf;
+    int currLayout=0;
+    
+    int backgroundColorLists[]= {
+    		R.drawable.gradient_dark_green,
+    		R.drawable.gradient_dark_orange,
+    		R.drawable.gradient_dark_pink,
+    		R.drawable.gradient_dark_purple,
+    		R.drawable.gradient_dark_teal,
+    };
+    
+    Handler handler = new Handler(){  
+    	  
+        public void handleMessage(Message msg) {  
+            switch (msg.what) {      
+            case 1:      
+            	Log.d("dd","timer");
+
+            	// 点击读取下一个视图元素
+                vf.showNext();
+               
+                
+                
+                final View l1 = findViewById(R.id.LinearLayoutmain1);
+                final View l2 = findViewById(R.id.LinearLayoutmain2);
+                
+                if(currLayout<4)
+                {
+                	l2.setBackgroundResource(backgroundColorLists[currLayout]);//当前颜色层设为最上层
+                	l2.setVisibility(View.VISIBLE);//设为显示
+                	l1.setBackgroundResource(backgroundColorLists[currLayout+1]);//设置下一个层的背景色
+                    currLayout++;
+                }
+                else //==4
+                {	
+                	l2.setBackgroundResource(backgroundColorLists[currLayout]);//当前颜色层设为最上层
+                	l2.setVisibility(View.VISIBLE);//设为显示
+                	l1.setBackgroundResource(backgroundColorLists[0]);//设置下一个层的背景色
+                	currLayout=0;
+                }
+           
+                 
+                final Animation fadeOut = AnimationUtils.loadAnimation(FirstActivity.this,R.anim.fade_out);
+                final Animation fadeIn = AnimationUtils.loadAnimation(FirstActivity.this,R.anim.fade_in);
+
+                fadeOut.setAnimationListener(new AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        l2.setVisibility(View.GONE);
+                    }
+                });
+                l2.startAnimation(fadeOut);
+                l1.setVisibility(View.VISIBLE);
+                l1.startAnimation(fadeIn);
+    
+            	  
+                break;      
+            }      
+            super.handleMessage(msg);  
+        }  
+          
+    };  
+    
+    
+    TimerTask task = new TimerTask(){  
+        public void run() {  
+        	Log.d("dd","timer2");
+            Message message = new Message();      
+            message.what = 1;  
+            handler.sendMessage(message);    
+        }
+    };
+          
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		 //Remove title bar
+	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.first_page); 
+		
+	     // 定义ViewFlipper
+        vf = (ViewFlipper) findViewById(R.id.viewFlipper1);
+
+        // 设置切入动画
+        vf.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right));
+        // 设置切出动画
+        vf.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_left));
+
+         
+        timer.schedule(task, 0,2500);
+        
+        
+        
+        /*
+		setContentView(R.layout.join); 
+		Button button = (Button)findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {  
+		    @Override
+		    public void onClick(View v) {
+		        Intent intent = new Intent();
+		        intent.setClass(MainActivity.this, PageBActivity.class);
+		        startActivity(intent);
+		        //设置切换动画，从右边进入，左边退出
+		        overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);                
+		    }
+		});
+        */
+        
+	}
+
+
+}
--- src/com/smartcare/healthapp/MainActivity.java
+++ src/com/smartcare/healthapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
@@ -20,52 +21,12 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main); 
-		
-	     // 定义ViewFlipper
-        final ViewFlipper vf = (ViewFlipper) findViewById(R.id.viewFlipper1);
-
-        // 设置点击监听器
-        vf.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                        // 点击读取下一个视图元素
-                        vf.showNext();
-                        
-
-                        final View l1 = findViewById(R.id.LinearLayoutmain3);
-                        final View l2 = findViewById(R.id.LinearLayoutmain2);
-
-                        final Animation fadeOut = AnimationUtils.loadAnimation(MainActivity.this,android.R.anim.fade_out);
-                        final Animation fadeIn = AnimationUtils.loadAnimation(MainActivity.this,android.R.anim.fade_in);
-
-                                fadeOut.setAnimationListener(new AnimationListener() {
-                                    @Override
-                                    public void onAnimationStart(Animation animation) {
-                                    }
-
-                                    @Override
-                                    public void onAnimationRepeat(Animation animation) {
-                                    }
-
-                                    @Override
-                                    public void onAnimationEnd(Animation animation) {
-                                        l1.setVisibility(View.GONE);
-                                    }
-                                });
-                                l1.startAnimation(fadeOut);
-                                l2.setVisibility(View.VISIBLE);
-                                l2.startAnimation(fadeIn);
-                }
-        });
-
-        // 设置切入动画
-        vf.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right));
-        // 设置切出动画
-        vf.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_left));
+		 //Remove title bar
+	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    
+		setContentView(R.layout.join); 
 
         /*
-		setContentView(R.layout.join); 
 		Button button = (Button)findViewById(R.id.button1);
 		button.setOnClickListener(new View.OnClickListener() {  
 		    @Override
